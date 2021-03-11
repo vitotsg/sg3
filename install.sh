9861cf88809b0a209a4c0a647490c799948d7dbc
Todor Grigorov <t.grigorov@gmail.com>
2021-03-11 06:19:10 -0500

    fixed typo

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@ yum install -y sg3_utils smartmontools terminator
 
 mkdir -p /root/.config && chmod 700 /root/.config
 cp -R /root/sg3/terminator /root/.config/
+
+terminator --layout=sg3 --profile=sg3 --config=/root/.config/terminator/config
