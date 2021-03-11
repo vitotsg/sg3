9c167e547d0e8ffaa2f91bc8d0be7864df0816e2
Todor Grigorov <t.grigorov@gmail.com>
2021-03-11 06:14:51 -0500

    fixed config

--- install.sh
+++ install.sh
@@ -1,1 +1,4 @@
-yum install -y sg3_utils smartmontools
+yum install -y sg3_utils smartmontools terminator
+
+mkdir -p /root/.config && chmod 700 /root/.config
+cp -R /root/sg3/terminator /root/.config/
