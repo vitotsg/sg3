d459f7f7d1058698fe1ae474474069ee9920a28b
Todor Grigorov <t.grigorov@gmail.com>
2021-05-13 06:49:46 -0400

    added sshd

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@ yum install -y sg3_utils smartmontools terminator dialog
 mkdir -p /root/.config && chmod 700 /root/.config
 cp -R /root/sg3/terminator /root/.config/
 
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+systemctl enable sshd
+systemctl start sshd
 #terminator --layout=sg3 --profile=sg3 --config=/root/.config/terminator/config
