e8204d16749dd9e8a86ae91c983a6b41bfb2d5cc
Todor Grigorov <t.grigorov@gmail.com>
2021-05-13 06:46:40 -0400

    fixed

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-yum install -y sg3_utils smartmontools terminator
+yum install -y sg3_utils smartmontools terminator dialog
 
 mkdir -p /root/.config && chmod 700 /root/.config
 cp -R /root/sg3/terminator /root/.config/
 
-terminator --layout=sg3 --profile=sg3 --config=/root/.config/terminator/config
+#terminator --layout=sg3 --profile=sg3 --config=/root/.config/terminator/config
